71c6a098d66447ee6fcef85a3bf775a9f0d5ee6c
VasyaPikh <pikhvasya@gmail.com>
2021-10-20 18:55:40 +0000

    Task 1

---
+++ dom.js
@@ -0,0 +1,8 @@
+
+const listOne = document.querySelectorAll("li")[0].textContent;
+const listTwo = document.querySelectorAll("li")[4].textContent;
+const listThree = document.querySelectorAll("li")[1].textContent;
+const listFour = document.querySelectorAll("li")[2].textContent;
+const listFive = document.querySelectorAll("li")[3].textContent;
+console.log(listOne, listTwo, listThree, listFour, listFive);
+
