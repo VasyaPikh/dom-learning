1660e1502d29bb980ccc750abd51a8910a97a0cf
VasyaPikh <pikhvasya@gmail.com>
2021-10-20 19:53:12 +0000

    Task 2

--- task-one/task1.js
+++ task-one/task1.js
@@ -1,4 +1,3 @@
-
 const listOne = document.querySelectorAll("li")[0].textContent;
 const listTwo = document.querySelectorAll("li")[4].textContent;
 const listThree = document.querySelectorAll("li")[1].textContent;
@@ -6,3 +5,5 @@ const listFour = document.querySelectorAll("li")[2].textContent;
 const listFive = document.querySelectorAll("li")[3].textContent;
 console.log(listOne, listTwo, listThree, listFour, listFive);
 
+// var str = "Hello World!";
+// var result = str.bold();
